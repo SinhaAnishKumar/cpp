7ab07326ece14002f933b45f88ec1b2908153feb
Anish Kumar Sinha <sinhanish.rajan@gmail.com>
2021-03-24 00:56:30 +0530

    Update quartile_deviation.cpp

--- quartile_deviation.cpp
+++ quartile_deviation.cpp
@@ -1,40 +1,44 @@
 //Write a program to find Quartile deviation of an ungrouped data.
 
 #include <iostream>
-#include <algorithm>
-
 using namespace std;
 
 int main()
 {
-    int arr[] = {20, 35, 25, 12, 10, 23, 18, 14, 30, 40};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    sort(arr, arr + n);
-
-    // for (int i = 0; i < n; i++)
-    // {
-    //     cout << arr[i] << " ";
-    // }
-
-    float q1, q3, qd;
-    float q1_pos, q3_pos;
-    q1_pos = (n+1)/4.0;
-    q3_pos = (3 * (n+1))/4.0 ;
-
-
-    q1 = arr[(int)q1_pos - 1] + (((int)(q1_pos*100) % 100) /100.0 * (arr[(int)q1_pos] - arr[(int)q1_pos - 1] )) ; 
-    
-    q3 = arr[(int)q3_pos - 1] + (((int)(q3_pos*100) % 100) /100.0 * (arr[(int)q3_pos] - arr[(int)q3_pos - 1] )) ;
-
-
-    // cout << "\nQ1 = " << q1 << endl;
-    // cout << "\nQ3 = " << q3 << endl;
-
-    qd = (q3 - q1) / 2.0;
-
-    cout << "\n (semi-interquartile range) or Quartile Deviation = " << qd << endl;
-
-    return 0;
-
+     int a[100], i, t, j, n1, n3, n;
+     double q1, q3, qd;
+     cout<<"Enter Number Of Items FIrst : ";
+     cin>>n;
+     cout << "\nNow! Enter " <<n <<" Values : ";
+     for (i = 0; i < n; i++)
+     {
+          cin >> a[i];
+     }
+     for (i = 0; i < n; i++)
+     {
+          for (j = 0; j < n - 1; j++)
+          {
+               if (a[j] > a[j + 1])
+               {
+                    t = a[j];
+                    a[j] = a[j + 1];
+                    a[j + 1] = t;
+               }
+          }
+     }
+     cout <<"\nSorted Array Below :)";
+     for (i = 0; i < n; i++)
+     {
+          cout << "\n         "
+               << a[i];
+     }
+     n1 = (n + 1) * 0.25;
+     n3 = (n + 1) * 0.75;
+     q1 = a[n1 - 1];
+     q3 = a[n3 - 1];
+     qd = (q3 - q1) / 2;
+     cout << "\nLower Quartile = " << q1;
+     cout << "\nUpper Quartile = " << q3;
+     cout << "\nQUARTILE DEVIATION= " << qd;
+     return (0);
 }
